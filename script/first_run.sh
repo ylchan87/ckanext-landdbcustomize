bf8ffeaa5b272a998da11fb148766f85aaaaa2bc
ylchan87 <ylchan87@gmail.com>
2021-05-08 14:58:19 +0800

    move first_run.sh content to dockerfile

--- script/first_run.sh
+++
@@ -1,5 +0,0 @@
-source /usr/lib/ckan/venv/bin/activate #ckan python env
-
-ckan-pip3 install -e /usr/lib/ckan/venv/src/ckan/
-ckan-pip3 install -e /usr/lib/ckan/venv/src/ckanext-landdbcustomize/
-ckan-pip3 install flask_debugtoolbar
